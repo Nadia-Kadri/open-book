5d608aad985dbc530dfef3d59874c91543fd0c2b
Nadia-Kadri <nadiagrace96@gmail.com>
2024-03-20 12:53:17 -0400

    Create links to sort by title, date and rating

--- index.js
+++ index.js
@@ -21,15 +21,23 @@ app.get("/", async (req, res) => {
   // /?sort=title /?sort=date /?sort=rating
   const sort = req.query.sort;
   let result;
-  if (sort === "title") {
-    result = await db.query("SELECT * FROM book ORDER BY title ASC");
-  } else if (sort === "date") {
-    result = await db.query("SELECT * FROM book ORDER BY date_read DESC");
-  } else if (sort === "rating") {
-    result = await db.query("SELECT * FROM book ORDER BY rating DESC");
+  
+  if (sort) {
+    sort === "title" ? result = await db.query("SELECT * FROM book ORDER BY title ASC") : "";
+    sort === "date" ? result = await db.query("SELECT * FROM book ORDER BY date_read DESC") : "";
+    sort === "rating" ? result = await db.query("SELECT * FROM book ORDER BY rating DESC") : "";
   } else {
     result = await db.query("SELECT * FROM book");
   }
+  // if (sort === "title") {
+  //   result = await db.query("SELECT * FROM book ORDER BY title ASC");
+  // } else if (sort === "date") {
+  //   result = await db.query("SELECT * FROM book ORDER BY date_read DESC");
+  // } else if (sort === "rating") {
+  //   result = await db.query("SELECT * FROM book ORDER BY rating DESC");
+  // } else {
+  //   result = await db.query("SELECT * FROM book");
+  // }
   
   res.render("index.ejs", { books: result.rows })
 });
