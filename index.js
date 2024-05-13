11bf9f9f412394167cd4975477e5d635064f3b3a
Nadia-Kadri <nadiagrace96@gmail.com>
2024-05-13 08:16:17 -0400

    Create delete route to remove book from database

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/", async (req, res) => {
   // /?sort=title /?sort=date /?sort=rating
   const sort = req.query.sort;
   let result;
-  
+
   if (sort) {
     sort === "title" ? result = await db.query("SELECT * FROM book ORDER BY title ASC") : "";
     sort === "date" ? result = await db.query("SELECT * FROM book ORDER BY date_read DESC") : "";
@@ -29,17 +29,39 @@ app.get("/", async (req, res) => {
   } else {
     result = await db.query("SELECT * FROM book");
   }
-  // if (sort === "title") {
-  //   result = await db.query("SELECT * FROM book ORDER BY title ASC");
-  // } else if (sort === "date") {
-  //   result = await db.query("SELECT * FROM book ORDER BY date_read DESC");
-  // } else if (sort === "rating") {
-  //   result = await db.query("SELECT * FROM book ORDER BY rating DESC");
-  // } else {
-  //   result = await db.query("SELECT * FROM book");
-  // }
   
   res.render("index.ejs", { books: result.rows })
+});
+
+app.get("/new", async (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/new", async (req, res) => {
+  const book = req.body;
+  await db.query(
+    "INSERT INTO book (title, author, isbn, review, rating, date_read) VALUES ($1, $2, $3, $4, $5, $6)",
+    [book.title, book.author, book.isbn, book.review, book.rating, book.date_read]
+  );
+  console.log(book);
+  // res.send("Book added");
+  res.redirect("/?sort=date");
+});
+
+app.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await db.query(
+    "SELECT * FROM book WHERE id = $1",
+    [id]
+  );
+  const book = result.rows;
+
+  await db.query(
+    "DELETE FROM book WHERE id = $1",
+    [id]
+  );
+  
+  res.redirect("/");
 });
 
 app.listen(port, () => {
--- queries.sql
+++ queries.sql
@@ -1,17 +1,21 @@
+DROP TABLE IF EXISTS book;
+
 CREATE TABLE book (
 	id SERIAL PRIMARY KEY,
 	title VARCHAR(200) NOT NULL,
 	author VARCHAR(100) NOT NULL,
+  isbn BIGINT,
 	review TEXT,
   rating INT CHECK (rating >= 1 AND rating <= 10),
 	date_read DATE
 );
 
-INSERT INTO book (title, author, review, rating, date_read) 
+INSERT INTO book (title, author, isbn, review, rating, date_read) 
 VALUES 
 (
 	'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
 	'James Clear',
+  9781847941848,
 	'Atomic Habits is a must-read for anyone looking to make lasting improvements in their life. He introduces the idea of "atomic habits," which are tiny changes that compound over time to produce significant outcomes. By focusing on the small behaviors that shape our daily lives, Clear demonstrates how anyone can create lasting change and achieve their goals. One of the standout features of the book is Clears emphasis on the importance of environment and identity in shaping behavior. He explains how our surroundings and the way we perceive ourselves play a crucial role in habit formation, and provides strategies for optimizing both to support positive change.',
 	10,
 	'2023-02-01'
@@ -19,6 +23,7 @@ VALUES
 (
 	'The Psychology of Money: Timeless lessons on wealth, greed, and happiness',
 	'Morgan Housel',
+  9780857197689,
 	'The Psychology of Money is a captivating exploration of the complex relationship between human psychology and financial decision-making. Rather than focusing solely on traditional financial advice, Housel delves into the psychological factors that influence how people manage, invest, and think about money. Throughout the book, Housel emphasizes that understanding ones own psychology is just as important as understanding the mechanics of money. He illustrates this point through a series of compelling anecdotes, historical examples, and psychological insights, making the book both informative and engaging. Whether you are a seasoned investor or someone just starting to think about personal finance, this book provides valuable insights and guidance for navigating the complex world of money management.',
 	10,
  	'2024-01-01'
@@ -26,6 +31,7 @@ VALUES
 (
 	'Steve Jobs',
 	'Walter Isaacson',
+  9781477701461,
 	'"Steve Jobs" by Walter Isaacson is an in-depth biography that offers a comprehensive look into the life and career of one of the most influential figures in modern technology and business. Drawing from extensive interviews with Jobs himself, as well as interviews with family members, friends, colleagues, and competitors, Isaacson presents a nuanced portrait of the man behind the Apple empire. Whether you are a fan of Apple products or simply interested in the story of one of the most influential figures of the digital age, this biography provides a compelling narrative that sheds light on the man behind the myth.',
 	8,
  	'2024-03-01'
