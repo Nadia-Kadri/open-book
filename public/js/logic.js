86f95b0475bcfe315fb167edff17c0431aa53e44
Nadia-Kadri <nadiagrace96@gmail.com>
2024-05-21 11:38:27 -0400

    Create title search dropdown with event listeners to toggle visibility

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,39 +1,39 @@
 const ratingInputs = document.querySelectorAll(".rating-input");
-const ratingInputsArray = [...ratingInputs];
-const titleSearch = document.querySelector("#title");
+const ratingInputsArr = [...ratingInputs];
+const titleInput = document.querySelector("#title");
+const titleSearchDropdown = document.querySelector(".titleSearchDropdown");
 
 // Event Listeners
-titleSearch.addEventListener("keyup", handleTitleInputKeyDown);
-ratingInputs.forEach(element => {
-  element.addEventListener("click", handleRatingInputClick);
-});
+titleInput.addEventListener("keyup", handleTitleInputKeyUp);
 
-function handleTitleInputKeyDown(e) {
-  const searchInput = titleSearch.value.toLowerCase().replace(/\s/g, "+");
-  if (searchInput.length >= 3) {
-    openLibrarySearch(searchInput);
-  }
+titleInput.addEventListener("focusin", () => titleSearchDropdown.style.visibility = "visible");
+titleInput.addEventListener("focusout", () => titleSearchDropdown.style.visibility = "hidden");
+
+ratingInputs.forEach(el => el.addEventListener("click", handleRatingInputClick));
+
+function handleTitleInputKeyUp() {
+  const input = titleInput.value.toLowerCase().replace(/\s/g, "+");
+  input.length >= 3 ? openLibrarySearch(input) : null;
 }
 
 function handleRatingInputClick(e) {
   const selectedValue = parseFloat(e.currentTarget.getAttribute("value"));
 
-  ratingInputsArray.forEach(element => {
-    if (parseFloat(element.getAttribute("value")) <= selectedValue) {
-      element.classList.replace("fa-regular", "fa-solid");
+  ratingInputsArr.forEach(el => {
+    if (parseFloat(el.getAttribute("value")) <= selectedValue) {
+      el.classList.replace("fa-regular", "fa-solid");
     } else {
-      element.classList.replace("fa-solid", "fa-regular");
+      el.classList.replace("fa-solid", "fa-regular");
     }
   });
 }
 
 function openLibrarySearch(input) {
   axios.get('https://openlibrary.org/search.json?title=' + input + "&limit=5")
-  .then(function (response) {
-    // console.log(response.data.docs)
+  .then(res => {
     let bookArr = [];
 
-    response.data.docs.forEach(book => {
+    res.data.docs.forEach(book => {
       let bookObj = {
         title: book.title ? book.title : "",
         author: book.author_name ? book.author_name : "",
@@ -44,7 +44,5 @@ function openLibrarySearch(input) {
 
     console.log(bookArr);
   })
-  .catch(function (error) {
-    console.log(error);
-  });
+  .catch(err => console.log(err));
 }
